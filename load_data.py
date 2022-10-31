bbce91835059c8c86b27093d328e52db3942b679
alexmihalache <am.mihalache@gmail.com>
2022-10-31 22:05:11 +0000

    Refactoring

---
+++ load_data.py
@@ -0,0 +1,15 @@
+# load data
+import pandas as pd
+
+def load_data():
+    xt = pd.read_csv('data/xt.csv')
+    xt_test = pd.read_csv('data/xt_test.csv')
+    vaep = pd.read_csv('data/vaep.csv')
+    vaep_test = pd.read_csv('data/vaep_test.csv')
+
+    games = pd.read_csv('data/games.csv')
+    games_test = pd.read_csv('data/games_test.csv')
+    players = pd.read_csv('data/players.csv')
+    players_test = pd.read_csv('data/players_test.csv')
+
+    return xt, xt_test, vaep, vaep_test, games, games_test, players, players_test

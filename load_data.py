b07dcbf702658e12662ea1bc37d98c9afc95f3b4
alexmihalache <am.mihalache@gmail.com>
2022-11-06 00:48:06 +0000

    updated load_data with cloud links

--- load_data.py
+++ load_data.py
@@ -1,16 +1,31 @@
 # load data
 import pandas as pd
+import config 
 
 def load_data():
-    xt              = pd.read_csv('data/xt.csv')
-    xt_test         = pd.read_csv('data/xt_test.csv')
-    vaep            = pd.read_csv('data/vaep.csv')
-    vaep_test       = pd.read_csv('data/vaep_test.csv')
+    xt              = pd.read_csv(config.xt)
+    xt_test         = pd.read_csv(config.xt_test)
+    vaep            = pd.read_csv(config.vaep)
+    vaep_test       = pd.read_csv(config.vaep_test)
 
-    games           = pd.read_csv('data/games.csv')
-    games_test      = pd.read_csv('data/games_test.csv')
-    players         = pd.read_csv('data/players.csv')
-    players_test    = pd.read_csv('data/players_test.csv')
-    target_players  = pd.read_csv('data/target_players.csv')
+    games           = pd.read_csv(config.games)
+    games_test      = pd.read_csv(config.games_test)
+    players         = pd.read_csv(config.players)
+    players_test    = pd.read_csv(config.players_test)
+    target_players  = pd.read_csv(config.target_players)
 
     return xt, xt_test, vaep, vaep_test, games, games_test, players, players_test, target_players
+
+# def load_data():
+#     xt              = pd.read_csv('data/xt.csv')
+#     xt_test         = pd.read_csv('data/xt_test.csv')
+#     vaep            = pd.read_csv('data/vaep.csv')
+#     vaep_test       = pd.read_csv('data/vaep_test.csv')
+
+#     games           = pd.read_csv('data/games.csv')
+#     games_test      = pd.read_csv('data/games_test.csv')
+#     players         = pd.read_csv('data/players.csv')
+#     players_test    = pd.read_csv('data/players_test.csv')
+#     target_players  = pd.read_csv('data/target_players.csv')
+
+#     return xt, xt_test, vaep, vaep_test, games, games_test, players, players_test, target_players
